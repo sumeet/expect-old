3d30f9877c0970365da6613a85a486a3e0199846
Sumeet Agarwal <sumeet.a@gmail.com>
2012-07-31 01:42:20 -0700

    rename ocd

--- expecto.py
+++ expecto.py
@@ -11,16 +11,19 @@ def new_expecto(**methods):
 
 class Expecto:
 
-    def __init__(self, arg, **methods):
-        self._arg = arg
-        for name, method in methods.iteritems():
-            vars(self)[name] = partial(method, arg)
+    def __init__(self, arg_expect_called_with, **methods):
+        self._arg_expect_called_with = arg_expect_called_with
+        self._set_methods(methods, arg_expect_called_with)
 
     def stub(self, arg_name):
         def _(*args):
             raise ExpectationError
-        self._arg.classmethod = staticmethod(_)
+        self._arg_expect_called_with.classmethod = staticmethod(_)
         return self
 
     def with_(self, *args, **kwargs):
         return
+
+    def _set_methods(self, methods, arg_expect_called_with):
+        for name, method in methods.iteritems():
+            vars(self)[name] = partial(method, arg_expect_called_with)
