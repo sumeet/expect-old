9f92431dfe2911ec66c7eecf06c9070f183a2953
Sumeet Agarwal <sumeet.a@gmail.com>
2012-08-01 01:08:53 -0700

    ocd.

--- expecto.py
+++ expecto.py
@@ -11,19 +11,19 @@ def new_expecto(**methods):
 
 class Expecto:
 
-    def __init__(self, arg_expect_called_with, **methods):
-        self._arg_expect_called_with = arg_expect_called_with
-        self._set_methods(methods, arg_expect_called_with)
+    def __init__(self, argument, **methods):
+        self._argument = argument
+        self._set_methods(methods)
 
     def stub(self, arg_name):
         def _(*args):
             raise ExpectationError
-        self._arg_expect_called_with.classmethod = staticmethod(_)
+        self._argument.classmethod = staticmethod(_)
         return self
 
     def with_(self, *args, **kwargs):
         return
 
-    def _set_methods(self, methods, arg_expect_called_with):
+    def _set_methods(self, methods):
         for name, method in methods.iteritems():
-            vars(self)[name] = partial(method, arg_expect_called_with)
+            vars(self)[name] = partial(method, self._argument)
