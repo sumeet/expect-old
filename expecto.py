ebefa3e7e99adc8f6ce8353a60e81dcc1faa9111
Sumeet Agarwal <sumeet.a@gmail.com>
2012-08-05 16:45:21 -0700

    add mock expectations

--- expecto.py
+++ expecto.py
@@ -1,29 +1,86 @@
+from collections import namedtuple
 from functools import partial
 
-
-class ExpectationError(Exception):
-    pass
+from mock import patch
 
 
 def new_expecto(**methods):
-    return lambda arg: Expecto(arg, **methods)
+    return Expecto(**methods)
+
+
+Args = namedtuple('Args', 'args kwargs')
 
 
 class Expecto:
 
-    def __init__(self, argument, **methods):
+    def __init__(self, **methods):
+        self._methods = methods
+
+    def __call__(self, argument):
         self._argument = argument
-        self._set_methods(methods)
+        return self
 
     def stub(self, arg_name):
-        def _(*args):
-            raise ExpectationError
-        self._argument.classmethod = staticmethod(_)
+        self._expectation = StubExpectation(self._argument, arg_name)
+        return self
+
+    def should_receive(self, arg_name):
+        self._expectation = MockExpectation(self._argument, arg_name)
         return self
 
     def with_(self, *args, **kwargs):
-        return
+        self._expectation.set_args(*args, **kwargs)
+        return self
 
-    def _set_methods(self, methods):
-        for name, method in methods.iteritems():
-            vars(self)[name] = partial(method, self._argument)
+    def and_return(self, return_value):
+        self._expectation.set_return_value(return_value)
+
+    def verify(self):
+        self._expectation.verify()
+
+    def __getattr__(self, name):
+        return partial(self._methods[name], self._argument)
+
+
+class Stub(object):
+
+    def __init__(self, obj, name, verifier):
+        self._patcher = patch.object(obj, name)
+        self._stub = self._patcher.start()
+        self._verifier = verifier
+
+    def set_return_value(self, value):
+        self._stub.return_value = value
+
+    def set_args(self, *args, **kwargs):
+        self._with_args = Args(args, kwargs)
+
+    def assert_called_once_with_args(self):
+        self._stub.assert_called_once_with(*self._with_args.args,
+                                           **self._with_args.kwargs)
+
+    def verify(self):
+        self._verifier.verify(self)
+
+    @property
+    def was_called(self):
+        return self._stub.called
+
+
+class VerifiesStubs(object):
+
+    @classmethod
+    def verify(cls, stub):
+        if stub.was_called:
+            stub.assert_called_once_with_args()
+
+
+class VerifiesMocks(object):
+
+    @classmethod
+    def verify(cls, stub):
+        stub.assert_called_once_with_args()
+
+
+StubExpectation = lambda obj, name: Stub(obj, name, VerifiesStubs)
+MockExpectation = lambda obj, name: Stub(obj, name, VerifiesMocks)
--- test.py
+++ test.py
@@ -3,11 +3,17 @@ import unittest
 from expecter import expect
 from mock import Mock
 
-from expecto import ExpectationError
 from expecto import new_expecto
 
 
 class ExpectoTest(unittest.TestCase):
+
+    class MyClass(object):
+        @classmethod
+        def classmethod(cls): pass
+
+    def setUp(self):
+        self.expecto = new_expecto()
 
     def test_delegates_to_eq(self):
         assert_equal = Mock(name='assert_equal')
@@ -16,11 +22,24 @@ class ExpectoTest(unittest.TestCase):
         assert_equal.assert_called_once_with('a', 'b')
 
     def test_stub_with_wrong_argument_raises_exception(self):
-        class MyClass(object): pass
-        expecto = new_expecto()
-        expecto(MyClass).stub('classmethod').with_(1)
-        with expect.raises(ExpectationError):
-            MyClass.classmethod(2)
+        self.expecto(self.MyClass).stub('classmethod').with_(1)
+        self.MyClass.classmethod(2)
+        with expect.raises(AssertionError):
+            self.expecto.verify()
+
+    def test_stub_returns_argument(self):
+        self.expecto(self.MyClass).stub('classmethod').with_(1).and_return(2)
+        expect(self.MyClass.classmethod(1)) == 2
+
+    def test_stub_does_not_raise_exception_if_not_called(self):
+        self.expecto(self.MyClass).stub('classmethod').with_(1).and_return(2)
+        self.expecto.verify()
+
+    def test_mock_expectation_raises_exception_if_not_called(self):
+        (self.expecto(self.MyClass).should_receive('classmethod').with_(1)
+                                   .and_return(2))
+        with expect.raises(AssertionError):
+            self.expecto.verify()
 
 
 if __name__ == '__main__':
